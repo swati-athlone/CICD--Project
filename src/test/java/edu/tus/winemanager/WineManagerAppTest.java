6aaf9cb7ad750898009d9d11fb0a20015630d5b6
Swati Gupta <A00325336@student.tus.ie>
2025-04-24 12:36:07 +0100

    corrected tests for sonarqube issues

--- src/test/java/edu/tus/winemanager/WineManagerAppTest.java
+++ src/test/java/edu/tus/winemanager/WineManagerAppTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-public class WineManagerAppTest {
+class WineManagerAppTest {
 
     @Test
      void testMainMethod() {
