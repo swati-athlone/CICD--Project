380ed67417b50f05996734032aedc7f2b706eb55
Swati Gupta <A00325336@student.tus.ie>
2025-04-24 09:39:27 +0100

    added logs for webhook check

--- src/main/java/edu/tus/winemanager/controllers/WineService.java
+++ src/main/java/edu/tus/winemanager/controllers/WineService.java
@@ -46,7 +46,7 @@ public class WineService {
 			wineValidator.validateWine(wine);
 			log.info("Validated wines");
 			Wine savedWine=wineRepo.save(wine);
-			log.info("Saved wine in Database");
+			log.info("Saved wines in Database");
 			return ResponseEntity.status(HttpStatus.CREATED).body(savedWine);
 		}catch(WineException e) {
 			ErrorMessage errorMessage=new ErrorMessage(e.getMessage());
