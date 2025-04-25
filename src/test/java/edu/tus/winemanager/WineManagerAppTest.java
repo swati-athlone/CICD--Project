a8e0f9a09b7cb785a356227c3369875ebfa61042
Swati Gupta <A00325336@student.tus.ie>
2025-04-25 21:35:41 +0100

    docker implementation removing port

--- src/test/java/edu/tus/winemanager/WineManagerAppTest.java
+++ src/test/java/edu/tus/winemanager/WineManagerAppTest.java
@@ -1,9 +1,11 @@
 package edu.tus.winemanager;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WineManagerAppTest {
 
     @Test
